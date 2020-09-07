937e5f9f1ebe498c879b3f449bb20659687c35fe
Zac Smith <zrs3141592@gmail.com>
2020-09-06 22:41:30 -0600

    expand usability of function

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ const randomArray = []
 const arrLength = (range) => {
   return Math.floor(Math.random() * range) + 3
 }
-const arrValues = () => {
-  return Math.floor(Math.random() * 500) - 30
+const arrValues = (minV, maxV) => {
+  return Math.floor(Math.random() * maxV) - Math.abs(minV)
 }
 
-const createRandomArray = (n, cb = arrValues) => {
+const createRandomArray = (n, minV = 30, maxV = 500, cb = arrValues) => {
   if (typeof n !== 'number' || n <= 1 || n == null) {
     n = 10
   }
 
   let max = arrLength(n)
   for (let i = 0; i < max; i++) {
-    let ran = cb()
+    let ran = cb(minV, maxV)
     randomArray.push(ran)
   }
   return randomArray
