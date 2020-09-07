ecf18e39239fecba70fc8818be637cf4be1762b8
Zac Smith <zrs3141592@gmail.com>
2020-09-06 22:41:40 -0600

    update

--- test.js
+++ test.js
@@ -1,6 +1,9 @@
 const createRandomArray = require('./index.js')
 
-console.table(createRandomArray('bob'))
-console.table(createRandomArray(20))
-console.table(createRandomArray(-3))
-console.table(createRandomArray())
+// -------- Uncomment the confirmation you need, as running them all together only builds redundant lists
+
+// console.table(createRandomArray('bob'))
+// console.table(createRandomArray(20, 10, 25))
+// console.table(createRandomArray(20, 50, 150))
+// console.table(createRandomArray(-3))
+// console.table(createRandomArray())
