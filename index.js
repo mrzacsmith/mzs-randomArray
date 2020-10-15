dc7416a54a816dc53524d8244639f6151ec476f2
garybot <argot.pryer@gmail.com>
2020-10-14 23:20:05 -0700

    fix arrValues range.

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const arrLength = (range) => {
   return Math.floor(Math.random() * range) + 3
 }
 const arrValues = (minV, maxV) => {
-  return Math.floor(Math.random() * maxV) - Math.abs(minV)
+  return Math.floor(Math.random() * (maxV - minV)) + minV
 }
 
-const createRandomArray = (n, minV = 30, maxV = 500, cb = arrValues) => {
+const createRandomArray = (n, minV = -30, maxV = 500, cb = arrValues) => {
   if (typeof n !== 'number' || n <= 1 || n == null) {
     n = 10
   }
