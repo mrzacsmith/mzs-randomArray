7aead5d87c2bd6b3120bab257a2312b9227c0154
Zac Smith <zrs3141592@gmail.com>
2020-10-17 11:00:58 -0600

    make negative range

--- index.js
+++ index.js
@@ -4,28 +4,28 @@ const arrLength = (range) => {
   return Math.floor(Math.random() * range) + 3
 }
 const arrValues = (minV, maxV) => {
-  return Math.floor(Math.random() * (maxV - minV)) + minV
+  return Math.floor(Math.random() * (maxV - minV)) - minV
 }
 
 const createRandomArray = (n, minV = 30, maxV = 500, cb = arrValues) => {
-  if (!Number.isInteger(n) || n <= 1 ) {
-    console.log("n must be an integer greater than 1.")
+  if (!Number.isInteger(n) || n <= 1) {
+    console.log('n must be an integer greater than 1.')
 
     n = 10
   }
 
   if (!Number.isInteger(maxV)) {
-    console.log("maxV must be an integer.")
+    console.log('maxV must be an integer.')
     maxV = 500
   }
 
   if (!Number.isInteger(minV)) {
-    console.log("minV must be an integer.")
+    console.log('minV must be an integer.')
     minV = 30
   }
 
   if (minV >= maxV) {
-    console.log("minV must be less than maxV.")
+    console.log('minV must be less than maxV.')
     minV = 30
     maxV = 500
   }
